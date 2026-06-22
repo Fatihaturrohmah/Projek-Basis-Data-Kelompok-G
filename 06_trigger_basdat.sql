ff4404e3ae7b0031f2b4f86bb43aad5cc7c41e93
SALMAN ALFARISY <salman.alfarisy@mhs.unsoed.ac.id>
2026-06-22 22:49:18 +0700

    Update 06_trigger_basdat.sql

--- 06_trigger_basdat.sql
+++ 06_trigger_basdat.sql
@@ -1,1 +1,11 @@
+CREATE TABLE log_keamanan (
+    id_log INT AUTO_INCREMENT PRIMARY KEY,
+    id_pelanggan VARCHAR(10),
+    aksi VARCHAR(50),
+    tanggal_kejadian DATETIME DEFAULT NOW()
+);
+
+DELIMITER $$
+
+CREATE TRIGGER trg_audit_perubahan_password
 
