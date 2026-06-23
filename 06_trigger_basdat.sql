9cd554451e02e95ea9c7f9f2802bfbbc42ce11f1
SALMAN ALFARISY <salman.alfarisy@mhs.unsoed.ac.id>
2026-06-23 07:14:58 +0700

    Update 06_trigger_basdat.sql

--- 06_trigger_basdat.sql
+++ 06_trigger_basdat.sql
@@ -8,4 +8,17 @@ CREATE TABLE log_keamanan (
 DELIMITER $$
 
 CREATE TRIGGER trg_audit_perubahan_password
+AFTER UPDATE ON pelanggan
+FOR EACH ROW
+BEGIN
+    -- Logika: Hanya mencatat JIKA kolom password mengalami perubahan
+    IF OLD.password <> NEW.password THEN
+        INSERT INTO log_keamanan (id_pelanggan, aksi)
+        VALUES (NEW.id_pelanggan, 'Pelanggan mengganti password akun');
+    END IF;
+END$$
 
+DELIMITER ;
+
+UPDATE pelanggan SET password = 'password_baru_123' WHERE id_pelanggan = 'PLG-01';
+SELECT * FROM log_keamanan;
